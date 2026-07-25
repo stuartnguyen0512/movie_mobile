eb03037f8793c48e6d21cae994330ff550a5f1d3
stuartnguyen0512 <minnguyen051201@gmail.com>
2026-07-25 18:33:00 -0230

    Add NativeWind, Tabs navigation, and movie details route
    - Install and configure NativeWind + TailwindCSS
    - Add (tabs) route group with Home/Profile/Saved/Search screens
    - Add movies/[id] dynamic route for movie details
    - Add assets (icons, images, fonts) and constants
    - Fix root layout Stack.Screen naming

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Profile = () => {
+  return (
+    <View className="flex-1 items-center justify-center ">
+      <Text className="text-5xl text-orange-500 text-bold">Profile</Text>
+    </View>
+  );
+};
+
+export default Profile;
+
+const styles = StyleSheet.create({});
---
+++ app/movies/[id].tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetails = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default MovieDetails;
+
+const styles = StyleSheet.create({});
